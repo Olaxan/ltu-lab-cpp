2fc05fb0275536907417b13306f8e230b0245c55
Fredrik Lind <34612802+Olaxan@users.noreply.github.com>
2018-11-11 23:54:38 +0100

    Rename HelloWorld.cpp to helloworld.cpp

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <sstream>
+
+#include "utilslib.h"
+
+using namespace std;
+using namespace efiilj;
+
+int main()
+{
+
+	cout << "Hello World!\n";
+	cout << "How many times do you wish to greet the world?" << endl;
+
+	int num = IOUtils::askInt("> ", "Please enter an integer.");
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << "Hello world" << string(i + 1, '!') << endl;
+	}
+
+}
