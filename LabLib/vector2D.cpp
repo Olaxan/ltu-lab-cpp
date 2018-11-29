cf4925c14d262c60615fd797eb1a4d937e6938fb
Fredrik Lind <fredrik.lind.96@gmail.com>
2018-11-29 19:04:53 +0100

    Added function and constructor declarations to Vector assignment.

--- LabLib/vector2D.cpp
+++ LabLib/vector2D.cpp
@@ -5,7 +5,49 @@ namespace efiilj
 
 	Vector2D::Vector2D() { }
 
+	Vector2D::Vector2D(const Vector2D& copy)
+	{ 
+		//copy.x1( this->x1() );
+	}
 
+	Vector2D::Vector2D(float x1, float y1, float x2, float y2)
+	{
+	}
+
+	Vector2D Vector2D::operator+(Vector2D& vect)
+	{
+		return Vector2D();
+	}
+
+	Vector2D Vector2D::operator-(Vector2D& vect)
+	{
+		return Vector2D();
+	}
+
+	Vector2D Vector2D::operator*(Vector2D& vect)
+	{
+		return Vector2D();
+	}
+
+	Vector2D Vector2D::operator==(Vector2D& vect)
+	{
+		return Vector2D();
+	}
+
+	Vector2D Vector2D::operator!=(Vector2D& vect)
+	{
+		return Vector2D();
+	}
+
+	float Vector2D::dot(Vector2D& vector) const
+	{
+		return 0.0f;
+	}
+
+	float Vector2D::norm() const
+	{
+		return 0.0f;
+	}
 
 	Vector2D::~Vector2D() { }
 
--- LabLib/vector2D.h
+++ LabLib/vector2D.h
@@ -6,11 +6,35 @@ namespace efiilj {
 	{
 	public:
 		Vector2D();
+		Vector2D(const Vector2D &copy);
+		Vector2D(float x1, float y1, float x2, float y2);
+
+		Vector2D operator + (Vector2D &vect);
+		Vector2D operator - (Vector2D &vect);
+		Vector2D operator * (Vector2D &vect);
+
+		Vector2D operator == (Vector2D &vect);
+		Vector2D operator != (Vector2D &vect);
+
+		const float& x1() const { return _arr[0]; }
+		void x1(const float& x1) { this->_arr[0] = x1; }
+
+		const float& y1() const { return _arr[1]; }
+		void y1(const float& y1) { this->_arr[1] = y1; }
+
+		const float& x2() const { return _arr[2]; }
+		void x2(const float& x2) { this->_arr[2] = x2; }
+
+		const float& y2() const { return _arr[3]; }
+		void y2(const float& y2) { this->_arr[3] = y2; }
+
+		float dot(Vector2D &vector) const;
+		float norm() const;
 
 		~Vector2D();
 
 	private:
-		int* arr[4] = { 0,0,0,0 };
+		float _arr[4];
 	};
 
 }
